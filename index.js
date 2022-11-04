1037592f11656aa41c648206a505ba2fcaa71b75
SRai22 <shravansomashekara.rai@gmail.com>
2022-11-04 10:33:09 -0700

    Call cpp function in js and run

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Calculator from './Calculator.js';
+import CalculatorWASM from './Calculator.wasm';
+
+const calculator = Calculator({
+    locateFile: () => {
+        return CalculatorWASM;
+    }
+});
+
+calculator.then((core) => {
+    console.log(core.add(1, 2));
+});
+
 ReactDOM.render(
     <div>
         <h1>react-cpp-wasm-calculator-demo</h1>
